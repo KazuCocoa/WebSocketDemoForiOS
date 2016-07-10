af2576fe047c3ed8f6b11d0c954cd7c1121ad991
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-07-10 23:03:03 +0900

    introduce WebSocket for Phoenix

--- Podfile
+++ Podfile
@@ -1,7 +1,9 @@
 # Uncomment this line to define a global platform for your project
-# platform :ios, '9.0'
+platform :ios, '9.0'
 # Uncomment this line if you're using Swift
-# use_frameworks!
+use_frameworks!
+
+pod 'SwiftPhoenixClient'
 
 target 'websocket-demo' do
 
--- websocket-demo/ViewController.swift
+++ websocket-demo/ViewController.swift
@@ -1,10 +1,18 @@
 import UIKit
+import SwiftPhoenixClient
 
 class ViewController: UIViewController {
+
+    let socket = Phoenix.Socket(domainAndPort: "localhost:4000", path: "socket", transport: "websocket")
+    let topic = "my_room:lobby"
+
+    let newMessageEvent = "new_message"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        setUpWebSocket()
     }
 
     override func didReceiveMemoryWarning() {
@@ -12,6 +20,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func setUpWebSocket() {
+        socket.join(topic: self.topic, message: Phoenix.Message(subject: "status", body: "joining")) { channel in
+            let chan = channel as! Phoenix.Channel
 
+            chan.on("join") { message in
+                print("You joined the room.\n")
+            }
+
+            chan.on(self.newMessageEvent) { message in
+                print("\(self.newMessageEvent): \(message)")
+            }
+
+            chan.on("user:entered") { message in
+                print("user:entered: \(message)")
+            }
+
+            chan.on("error") { message in
+                print("error: \(message)")
+            }
+        }
+    }
+
+    @IBAction func sendMessage(sender: AnyObject) {
+        let message = Phoenix.Message(message: ["name": "kazu cocoa", "message": "hello, phoenix"])
+        let payload = Phoenix.Payload(topic: self.topic, event: self.newMessageEvent, message: message)
+        self.socket.send(payload)
+    }
 }
-
